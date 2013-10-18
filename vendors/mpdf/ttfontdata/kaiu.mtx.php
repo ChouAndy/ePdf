eaef31d7062bcace9f4012aa3414ef370fe72308
ChouAndy <chouandy625@gmail.com>
2013-10-18 20:08:18 +0800

    New create.

---
+++ vendors/mpdf/ttfontdata/kaiu.mtx.php
@@ -0,0 +1,26 @@
+<?php
+$name='DFKaiShu-SB-Estd-BF';
+$type='TTF';
+$desc=array (
+  'Ascent' => 801,
+  'Descent' => -199,
+  'CapHeight' => 801,
+  'Flags' => 5,
+  'FontBBox' => '[0 -199 1000 801]',
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 1000,
+);
+$up=-107;
+$ut=49;
+$ttffile='C:/Windows/Fonts/kaiu.ttf';
+$TTCfontID='0';
+$originalsize=5178844;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='kaiu';
+$panose=' a 7 3 0 5 9 0 0 0 0 0 0';
+$haskerninfo=false;
+$unAGlyphs=false;
+?>
